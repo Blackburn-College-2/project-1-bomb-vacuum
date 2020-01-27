50a523390866922e4518aaa27dbd65b85553dace
Lane <laney.satchwell@gmail.com>
2020-01-26 20:24:15 -0600

    Made a grid of buttons. Yeety.

--- src/main/java/project/bomb/vacuum/GUI.java
+++ src/main/java/project/bomb/vacuum/GUI.java
@@ -1,9 +1,12 @@
 package project.bomb.vacuum;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
@@ -11,14 +14,43 @@ public class GUI extends Application {
     public static void launchGUI() {
         launch();
     }
-
-
+    
+    private static final int buttonSize = 50;
+    
     @Override
     public void start(Stage stage) throws Exception {
-
-        Pane pane = new BorderPane();
-        Scene scene = new Scene(pane, 800, 600);
+        Button[] gridbuttons = new Button[50];
+        int gridColumns = 10;
+        int gridRows = 10;
+        GridPane gridPane = new GridPane();
+        populateGrid(gridPane, gridColumns, gridRows);
+        
+        double screenHeight = (GUI.buttonSize * gridRows);
+        double screenWidth = (GUI.buttonSize * gridColumns);
+        
+        
+        Scene scene = new Scene(gridPane, screenWidth, screenHeight);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private Button makeButton() {
+        Button button = new Button();
+        button.setPrefSize(GUI.buttonSize,GUI.buttonSize);
+        return button;
+    }
+    
+    private void populateGrid(GridPane gridpane, int gridColumns, int gridRows){
+        for (int i = 0; i < gridColumns; i++) {
+            for (int j = 0; j < gridRows; j++) {
+                Button button = makeButton();
+                placeButton(button, gridpane, i, j);
+            }
+            
+        }
+    }
+    
+    private void placeButton(Button button, GridPane gridpane, int gridColumn, int gridRow){
+        gridpane.add(button, gridColumn, gridRow, 1, 1);
     }
 }
