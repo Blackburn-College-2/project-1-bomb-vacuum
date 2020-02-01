f0cfe640104a223fafea3108ca732fb20b8f8297
CordellStocker <cstocker@karmak.com>
2020-01-31 20:26:30 -0600

    updated controller, model, and view interfaces

--- src/main/java/project/bomb/vacuum/View.java
+++ src/main/java/project/bomb/vacuum/View.java
@@ -1,5 +1,34 @@
 package project.bomb.vacuum;
 
+/**
+ * Handles displaying the state of the game to the user.
+ */
 public interface View {
 
+    /**
+     * Creates a new board of tiles using the specified size.
+     *
+     * @param rows number of rows in the tile grid.
+     * @param columns number of columns in the tile grid.
+     */
+    void initializeBoard(int rows, int columns);
+
+    /**
+     * Sets the state of the specified tiles.
+     *
+     * @param tileStates the tiles that need updated in the view.
+     */
+    void setTileStatuses(TileStatus[] tileStates);
+
+    /**
+     * The user has won or lost, let them know!
+     */
+    void gameOver(GameOverState gameOverState, long time);
+
+    /**
+     * Set the displayed time.
+     *
+     * @param time the time to display.
+     */
+    void setTime(long time);
 }
