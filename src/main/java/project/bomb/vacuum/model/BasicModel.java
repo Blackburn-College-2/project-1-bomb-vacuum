601706b3fa1b9b30bdc55fb496ff56755ee06bd5
Tyler Braden <tyler.braden@blackburn.edu>
2020-02-02 12:07:33 -0600

    Added gameState Grid

--- src/main/java/project/bomb/vacuum/model/BasicModel.java
+++ src/main/java/project/bomb/vacuum/model/BasicModel.java
@@ -6,9 +6,17 @@ import project.bomb.vacuum.TileAction;
 import project.bomb.vacuum.Position;
 
 public class BasicModel implements Model {
+
+    private TileValue[][] gameState;
+
     @Override
     public void tileUpdatedByUser(TileAction tileAction, Position position) {
         throw new UnsupportedOperationException("Method not yet implemented.");
+    }
+
+    public static void main(String[] args) {
+        BasicModel n = new BasicModel();
+        n.newGame(DefaultBoard.INTERMEDIATE);
     }
 
     @Override
@@ -26,10 +34,114 @@ public class BasicModel implements Model {
         }
     }
 
+    /**
+     * So far, newGame will build the grid and place bombs. Next action is to
+     * make it count the number of tiles touched by bombs.
+     *
+     * @param rows // The number of rows to assign to the grid
+     * @param columns // The number of columns to assign to the grid
+     * @param bombs // The number of bombs to be placed on the grid
+     */
+
     @Override
     public void newGame(int rows, int columns, int bombs) {
-        
-        throw new UnsupportedOperationException("Method not yet implemented.");
+        TileValue[][] state = new TileValue[rows][columns];
+        for (int a = 0; a < rows; a++) { // assigns EMPTY in order 
+            // to avoid NULL POINTER
+            for (int b = 0; b < columns; b++) {
+                state[a][b] = TileValue.EMPTY;
+            }
+        }
+        for (int i = 0; i < (bombs); i++) {
+            int x = (int) (Math.random() * ((rows - 1) + 1));
+            int y = (int) (Math.random() * ((columns - 1) + 1)); //Generates an 
+            //int from 0 to 
+            //max - 1
+
+            if (state[x][y].equals(TileValue.BOMB)) { // making sure there 
+                // isn't overlap of bomb
+                i--;
+            } else {
+                state[x][y] = TileValue.BOMB; // assigning bombs to tiles 
+            }
+            this.gameState = state;
+        }
+
+        updateNumberedTiles();
+        printGameState();
+
+//        for (int j = 0; j < rows; j++) { // testing the values of the tiles                       ----TESTS----
+//            for (int k = 0; k < columns; k++) {
+//                System.out.println(state[j][k]);
+//            }                                                                                                      
+//        }                                                                                                         
+//        int bombCount = 0;                                                                                        
+//        for (int j = 0; j < rows; j++) { // testing the number of bombs placed                    ---- TESTS -----
+//            for (int k = 0; k < columns; k++) {
+//                if (state[j][k].equals(TileValue.BOMB)) {
+//                    bombCount++;
+//                }
+//            }
+//        }
+//        throw new UnsupportedOperationException("Method not yet implemented.");                   ----TESTS-----
+    }
+
+    public void printGameState() {
+        for (int i = 0; i < this.gameState.length; i++) {
+            System.out.println("");
+            for (int j = 0; j < this.gameState[0].length; j++) {
+                System.out.print(String.format("%-6s", gameState[i][j]));
+            }
+        }
+    }
+
+    /**
+     * This method will iterate through the gameState Array and increment all
+     * tiles that touch a bomb
+     */
+    public void updateNumberedTiles() { // t[0].length == col.len // t.length == row.len
+
+        int rowLen = this.gameState.length - 1; // # of rows
+        int colLen = this.gameState[0].length - 1; // # of columns
+        System.out.println(rowLen);
+        System.out.println(colLen);
+        for (int i = 0; i < rowLen + 1; i++) {
+            for (int j = 0; j < colLen + 1; j++) {
+                int tempRow = i;
+                int tempCol = j;
+
+                if (gameState[i][j].equals(TileValue.BOMB)) {
+
+                    incrementTile(i - 1, j - 1);
+                    incrementTile(i - 1, j);
+                    incrementTile(i - 1, j + 1);
+                    incrementTile(i, j - 1);
+                    incrementTile(i, j + 1);
+                    incrementTile(i + 1, j - 1);
+                    incrementTile(i + 1, j);
+                    incrementTile(i + 1, j + 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * This method will allow you to increment the TileValue when the board is
+     * being updated for the controller
+     *
+     * @param row // The target row to be changed
+     * @param column // The target column to be changed
+     */
+    public void incrementTile(int row, int column) {
+        try {
+            if (this.gameState[row][column] == TileValue.BOMB) {
+                return;
+            }
+            int temp = this.gameState[row][column].ordinal();
+            this.gameState[row][column] = TileValue.values()[temp + 1];
+        } catch (IndexOutOfBoundsException ex) {
+            // ignore
+        }
     }
 
     @Override
