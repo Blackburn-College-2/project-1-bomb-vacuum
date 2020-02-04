a589686223b949826ca5419a08348bd34d74782d
Lane <laney.satchwell@gmail.com>
2020-02-03 19:45:00 -0600

    made gui changes

--- src/main/java/project/bomb/vacuum/view/GUI.java
+++ src/main/java/project/bomb/vacuum/view/GUI.java
@@ -8,9 +8,12 @@ import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import project.bomb.vacuum.GameOverState;
+import project.bomb.vacuum.TileStatus;
+import project.bomb.vacuum.View;
 import project.bomb.vacuum.view.BombPane;
 
-public class GUI extends Application {
+public class GUI extends Application implements View {
 
     public static void launchGUI() {
         launch();
@@ -24,6 +27,7 @@ public class GUI extends Application {
         double screenHeight = 600;
         Pane anchorPane  = new Pane();
         BorderPane borderPane = new BorderPane();
+        
         BombPane bombPane = new BombPane(10,10);
 
         borderPane.setCenter(bombPane);
@@ -33,6 +37,26 @@ public class GUI extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+    }
+
+    @Override
+    public void initializeBoard(int rows, int columns) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setTileStatuses(TileStatus[] tileStates) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void gameOver(GameOverState gameOverState, long time) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setTime(long time) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 
