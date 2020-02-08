cd544942420bc8ef7952f65c5c49b02c0f9deb23
Rylee-Wilson <rylee.wilson@blackburn.edu>
2020-02-08 16:12:14 -0600

    can read scores

--- src/main/java/project/bomb/vacuum/model/BasicModel.java
+++ src/main/java/project/bomb/vacuum/model/BasicModel.java
@@ -1,6 +1,14 @@
 package project.bomb.vacuum.model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import project.bomb.vacuum.*;
 import project.bomb.vacuum.exceptions.InvalidBoardConfiguration;
@@ -38,9 +46,9 @@ public class BasicModel implements Model {
     }
 
     /**
-     * @param rows    // The number of rows to assign to the grid
+     * @param rows // The number of rows to assign to the grid
      * @param columns // The number of columns to assign to the grid
-     * @param bombs   // The number of bombs to be placed on the grid
+     * @param bombs // The number of bombs to be placed on the grid
      */
     private void newGame(int rows, int columns, int bombs) {
         this.bombTiles.clear();
@@ -94,7 +102,7 @@ public class BasicModel implements Model {
      * This method will allow you to increment the TileValue when the board is
      * being updated for the controller
      *
-     * @param row    // The target row to be changed
+     * @param row // The target row to be changed
      * @param column // The target column to be changed
      */
     private void incrementTile(int row, int column) {
@@ -191,14 +199,85 @@ public class BasicModel implements Model {
         this.controller.gameOver(gameOverState);
     }
 
-    @Override
+//    @Override
+//    //Still need to replace lines after 5 and order them 
+//    public void updateHighScore(DefaultBoard board, String name, long time) {
+//        PrintWriter pw;
+//        File path = new File("/project-1-bomb-vacuum/src/main/java/project/bomb/vacuum/");
+//        BufferedReader br;
+//        String line;
+//        int count;
+//
+//        try {
+//            if (board == DefaultBoard.EASY) {
+//                
+//                br = new BufferedReader(new FileReader(path + "easyhighscores.txt"));
+//                line = br.readLine();
+//                if (line == null) {
+//                    count = 0;
+//                } else {
+//                    count = 1;
+//                }
+//                while (line != null) {
+//                    line = br.readLine();
+//                    count++;
+//                }
+//
+//                if (count < 5) {
+//                    pw = new PrintWriter(new FileWriter(path + "easyhighscores.txt"));
+//                    pw.write(name + "|" + time + "\n");
+//                } else {
+//                    //replace lines here
+//                }
+//            } else if (board == DefaultBoard.INTERMEDIATE) {
+//                pw = new PrintWriter(new FileWriter(path + "intermediatehighscores.txt"));
+//                pw.println(name + "|" + time + "\n");
+//            } else if (board == DefaultBoard.EXPERT) {
+//                pw = new PrintWriter(new FileWriter(path + "experthighscores.txt"));
+//                pw.println(name + "|" + time + "\n");
+//            }
+//        } catch (IOException e) {
+//            System.out.println("File does not exist!");
+//        }
+//    }
     public void updateHighScore(DefaultBoard board, String name, long time) {
-        throw new UnsupportedOperationException("Method not yet implemented.");
+//        String path = "/project-1-bomb-vacuum/src/main/java/project/bomb/vacuum/";
+//        if (board == DefaultBoard.EASY) {
+//            addToAndSortHighScores(board, path + "easyhighscores.txt", name, time);
+//        } else if (board == DefaultBoard.INTERMEDIATE) {
+//            addToAndSortHighScores(board, path + "intermediatehighscores.txt", name, time);
+//        } else if (board == DefaultBoard.EXPERT) {
+//            addToAndSortHighScores(board, path + "experthighscores.txt", name, time);
+//        }
+
     }
 
-    public void getHighScores() {
-        throw new UnsupportedOperationException("Method not yet implemented.");
-    }
+//    private void addToAndSortHighScores(DefaultBoard board, String file, String name, long time) {
+//        PrintWriter pw;
+//        String[] highScores;
+//        int count = 0;
+//        try {
+//            pw = new PrintWriter(new FileWriter(file));
+//            highScores = getScores(board);
+//            for (int i = 0; i < highScores.length; i++) {
+//                if (highScores[i] != null) {
+//                    count++;
+//                }
+//            }
+//            if (count < highScores.length) {
+//                pw.write(name + "|" + time + "\n");
+//                pw.close();
+//            } else {
+//                highScores[4] = highScores[4].replace(highScores[4], name + "|" + time + "\n");
+//                pw.write(highScores[4]);
+//                pw.close();
+//            }
+//            pw.write(Collections.sort(Arrays.asList(highScores)));
+//            pw.close();
+//        } catch (IOException e) {
+//            System.out.println("File does not exist!");
+//        }
+//    }
 
     @Override
     public void cheatToggled(boolean toggle) {
@@ -222,7 +301,91 @@ public class BasicModel implements Model {
 
     @Override
     public HighScores getScores(DefaultBoard board) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String[] rawScores = getScoresHelper(board);
+        
+        return new HighScores() {
+            @Override
+            public HighScore getFirst() {
+                return convertRawScore(rawScores[0]);
+            }
+
+            @Override
+            public HighScore getSecond() {
+                return convertRawScore(rawScores[1]);
+            }
+
+            @Override
+            public HighScore getThird() {
+                return convertRawScore(rawScores[2]);
+            }
+
+            @Override
+            public HighScore getFourth() {
+                return convertRawScore(rawScores[3]);
+            }
+
+            @Override
+            public HighScore getFifth() {
+                return convertRawScore(rawScores[4]);
+            }
+        };
+    }
+
+    private HighScore convertRawScore(String score) {
+        if (score == null) {
+            return new HighScore() {
+            public String getName() {
+                return "";
+            }
+
+            public long getTime() {
+                return 0;
+            }
+        };
+        }
+        String[] parts = score.split("|");
+        return new HighScore() {
+            public String getName() {
+                return parts[0];
+            }
+
+            public long getTime() {
+                return Long.parseLong(parts[1]);
+            }
+        };
+    }
+
+    private String[] getScoresHelper(DefaultBoard board) {
+        BufferedReader br;
+        File path = new File("/project-1-bomb-vacuum/src/main/java/project/bomb/vacuum/");
+        String line;
+        String[] highScores = new String[5];
+
+        try {
+            if (board == DefaultBoard.EASY) {
+                br = new BufferedReader(new FileReader(path + "easyhighscores.txt"));
+                for (int i = 0; i < highScores.length; i++) {
+                    line = br.readLine();
+                    highScores[i] = line;
+                }
+            } else if (board == DefaultBoard.INTERMEDIATE) {
+                br = new BufferedReader(new FileReader(path + "intermediatehighscores.txt"));
+                for (int i = 0; i < highScores.length; i++) {
+                    line = br.readLine();
+                    highScores[i] = line;
+                }
+            } else if (board == DefaultBoard.EXPERT) {
+                br = new BufferedReader(new FileReader(path + "experthighscores.txt"));
+                for (int i = 0; i < highScores.length; i++) {
+                    line = br.readLine();
+                    highScores[i] = line;
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("File does not exist!");
+        }
+        return highScores;
     }
 
     @Override
