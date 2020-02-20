5ba54a09de5e0b7faa0acf84ccf52d0329c49d1e
CordellStocker <cstocker@karmak.com>
2020-02-19 20:48:31 -0600

    display finished

--- src/main/java/project/bomb/vacuum/view/HighScoresAlert.java
+++ src/main/java/project/bomb/vacuum/view/HighScoresAlert.java
@@ -1,5 +1,6 @@
 package project.bomb.vacuum.view;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.control.Alert;
@@ -7,6 +8,7 @@ import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import project.bomb.vacuum.Controller;
 import project.bomb.vacuum.DefaultBoard;
 import project.bomb.vacuum.HighScore;
@@ -14,6 +16,8 @@ import project.bomb.vacuum.HighScore;
 public class HighScoresAlert {
 
     private final Controller controller;
+
+    private static final Font FONT = new Font("COURIER NEW", 12);
 
     public HighScoresAlert(Controller controller) {
         this.controller = controller;
@@ -24,9 +28,11 @@ public class HighScoresAlert {
         HBox highScorePane = new HBox();
         for (DefaultBoard board : DefaultBoard.values()) {
             List<HighScore> scores = controller.getScores(board);
-            String title = board.toString();
-            VBox vBox = new VBox(new Label(title));
+            Label title = new Label(" " + board.toString());
+            title.setFont(FONT);
+            VBox vBox = new VBox(title);
             vBox.getChildren().addAll(this.createScoreLabels(scores));
+            vBox.setMinWidth(125);
             highScorePane.getChildren().add(vBox);
         }
 
@@ -35,15 +41,23 @@ public class HighScoresAlert {
         alert.setTitle("High Scores");
         DialogPane dialogPane = alert.getDialogPane();
         dialogPane.getChildren().add(highScorePane);
+        dialogPane.setMinHeight(220);
+        dialogPane.setMinWidth(400);
+        highScorePane.translateYProperty().bind(dialogPane.heightProperty().multiply(0.35));
         alert.show();
     }
 
     private List<Label> createScoreLabels(List<HighScore> scores) {
         List<Label> list = new ArrayList<>();
         for (int i = 0; i < scores.size(); i++) {
+            if (i >= 5) {
+                break;
+            }
             HighScore score = scores.get(i);
-            String message = String.format("%-2d. %-4s%s", i, score.getName(), Util.formatTime(score.getTime()));
-            list.add(new Label(message));
+            String message = String.format("%2d. %-4s%s", (i + 1), score.getName(), Util.formatTime(score.getTime()));
+            Label label = new Label(message);
+            label.setFont(FONT);
+            list.add(label);
         }
         return list;
     }
