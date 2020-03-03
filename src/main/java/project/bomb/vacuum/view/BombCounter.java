8f9d2a478b586909e7228d01ba22192b0ebbe255
Isaac Garrett <isaac.garrett@blackburn.edu>
2020-03-03 09:47:07 -0600

    added documentation

--- src/main/java/project/bomb/vacuum/view/BombCounter.java
+++ src/main/java/project/bomb/vacuum/view/BombCounter.java
@@ -4,11 +4,16 @@ import javafx.application.Platform;
 import javafx.scene.control.Label;
 
 public class BombCounter extends Label {
-
+    /**
+     * sets the size of font being used for the bomb counter
+     */
     public BombCounter() {
         this.setStyle("-fx-font-size: 15px");
     }
-
+    /**
+     * updates the number of bombs on the board
+     * @param bombs the number of bombs 
+     */
     public void setCounter(int bombs) {
         Platform.runLater(() -> {
             this.setText("Bombs Remaining: " + bombs);
